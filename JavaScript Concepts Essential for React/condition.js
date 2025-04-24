b7c8e494dc7b703af6b876c51e7833e8b103c9c6
Rialahmed0 <rashedahommod0@gmail.com>
2025-04-24 23:34:59 +0600

    operator

---
+++ JavaScript Concepts Essential for React/condition.js
@@ -0,0 +1,17 @@
+// operator 
+// + - / * ;
+let num1 = 5;
+let num2 = 2;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 * num2);
+
+// > < == === >= <= 
+
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 < num2);
+console.log(num1 == '5');
+console.log(num1 === '5');
