791efbe833b56e9903547bfb8854c3adf1d480bd
Rialahmed0 <rashedahommod0@gmail.com>
2025-04-24 19:39:58 +0600

    array.js

---
+++ JavaScript Concepts Essential for React/array.js
@@ -0,0 +1,15 @@
+let arr = [1, 3, 4, 5];
+console.log(arr.length);
+console.log(arr.indexOf(4));
+console.log(arr[2] = 2);
+console.log(arr[1]);
+console.log(arr.includes(5));
+console.log(arr.push(6));
+console.log(arr.push(9));
+console.log(arr);
+console.log(arr.pop());
+console.log(arr.shift());
+console.log(arr.unshift(100));
+
+console.log(arr);
+
--- JavaScript Concepts Essential for React/variable.js
+++ JavaScript Concepts Essential for React/variable.js
@@ -1,2 +1,26 @@
 let name = 'rial ';
 console.log(name);
+console.log(typeof (name));
+let age = 25;
+console.log(age);
+console.log(typeof (age));
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof (isStudent));
+
+let strNumber = '45.64346';
+console.log(strNumber);
+let flaoatNumber = parseFloat(strNumber);
+console.log(flaoatNumber);
+let intNumber = parseInt(flaoatNumber);
+console.log(intNumber);
+
+let tomatoPrice = 78;
+let potatoPrice = 50;
+let chiliPrice = 80;
+
+let totalPrice = tomatoPrice + potatoPrice + chiliPrice;
+console.log(totalPrice);
+
+let averagePrice = (tomatoPrice + potatoPrice + chiliPrice) / 3;
+console.log(averagePrice);
