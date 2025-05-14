71675db442122f179309014d0d909fdde7a9d04e
Rialahmed0 <rashedahommod0@gmail.com>
2025-05-14 14:29:44 +0600

    arrow function

---
+++ JavaScript Concepts Essential for React/es6/arrow.js
@@ -0,0 +1,15 @@
+let add = (a, b) => a + b;
+let add1 = add(2, 3);
+console.log(add1);
+
+let square = a => a * a;
+let square1 = square(5);
+console.log(square1);
+
+let sum = (a, b) => {
+    let sums = a + b;
+    return sums;
+
+}
+let sum1 = sum(10, 20);
+console.log(sum1);
