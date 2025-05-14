f5c5f34c2efb8e53a150ec6f1e535452b5c2e545
Rialahmed0 <rashedahommod0@gmail.com>
2025-05-14 12:02:55 +0600

    conditon

---
+++ JavaScript Concepts Essential for React/fundamental 6/conditon.js
@@ -0,0 +1,19 @@
+let age = 15;
+if (age >= 18) {
+    console.log('you are adult ');
+}
+else {
+    console.log('you are not adult ');
+}
+
+if (age > 0) {
+    console.log('this is positive number ');
+}
+else if (age < 0) {
+    console.log('this is negative number ');
+}
+else {
+    console.log('this is zero ');
+}
+
+
