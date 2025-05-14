a9724241e54eac162e47e011c6b0ff5cc1ab83e9
Rialahmed0 <rashedahommod0@gmail.com>
2025-05-14 16:37:41 +0600

    distructuring

---
+++ JavaScript Concepts Essential for React/es6/distructuring.js
@@ -0,0 +1,9 @@
+let number = [1, 2, 3, 4];
+let [a, b, c] = number
+console.log(a, b, c);
+let myself = {
+    name: 'rial',
+    age: 25
+}
+let { name: myName, age } = myself;
+console.log(myName, age);
