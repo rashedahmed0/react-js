b8e0786915050b58a87c364ba6cdf8ee90350a0b
Rialahmed0 <rashedahommod0@gmail.com>
2025-05-15 20:38:09 +0600

    component and props

---
+++ React Core Concept/my-first-react-project/src/App.jsx
@@ -0,0 +1,88 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Device name='laptop' price='30000'></Device>
+      <Device name='mobile' price='20000'></Device>
+      <Device name='watch' price='2500'></Device>
+      <Person></Person>
+      <Person1></Person1>
+      <Student class='8' roll='24'></Student>
+    </>
+  )
+}
+
+
+function Device(props) {
+  console.log(props);
+  return (
+    <>
+      <h1>this device is : {props.name} and price is : {props.price} </h1>
+    </>
+  )
+}
+
+
+
+
+
+
+
+
+
+function Person() {
+  const age = 24;
+  return <h3 style={{
+    border: '2px solid white',
+    padding: '10px',
+    borderRadius: '10px',
+    margin: '10px'
+  }}>i am ahmed i am {age} years old </h3>
+}
+
+function Person1() {
+
+  const ages = [1, 2, 3, 4, 5];
+  const borr = {
+    border: '2px solid blue',
+    padding: '10px',
+    borderRadius: '10px',
+    margin: '10px'
+  }
+
+  return (
+
+    <>
+      <h1 style={borr}>{ages[0]} + {ages[4]} = {ages[0] + ages[4]}</h1>
+
+    </>
+
+  )
+
+
+
+}
+function Student(props) {
+  console.log(props);
+  let myself = { name: 'rashed ', age: 25 };
+  return (
+    < >
+      <div className='student'>
+        <h1>i am {myself.name} i am {myself.age} years old  </h1>
+        <p>class :{props.class}</p>
+        <p>roll : {props.roll}</p>
+
+      </div>
+    </>
+  )
+
+}
+
+
+export default App
